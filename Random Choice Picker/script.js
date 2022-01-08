924ae6bc929bc4c2b335607b387e184e2f8ce3a7
JJ <73240114+JJudge0@users.noreply.github.com>
2022-01-08 13:53:05 +0000

    Update script.js

--- Random Choice Picker/script.js
+++ Random Choice Picker/script.js
@@ -1,30 +1,32 @@
 const tagsEl=document.getElementById('tags')
 const textarea=document.getElementById('textarea')
-
+console.log('ELLO')
 textarea.focus()
 
-textarea.addEventListener('keyup' , (e) => {
-    createTags(e.target.value);
+textarea.addEventListener('keyup' ,(e) => {
+    createTags(e.target.value)
 
     if(e.key === 'Enter') {
      setTimeout(() => {
        e.target.value=''
-     }, 10 );
-       randomSelect();
+     }, 10 )
+       randomSelect()
     }
 
-});
+})
 
 function createTags(input) {
-const tags = input.split(',').filter(tag => tag.trim() != '').map(tag => tag.trim())
 
-    tagsEl.innerHTML ='';
+const tags = input.split(',').filter(tag => tag.trim()   //Removes white space and if another comma is added then it won't add on to the array list
+!== '').map(tag => tag.trim())
+// console.log(tags)
+   tagsEl.innerHTML ='';
 
       tags.forEach(tag => {
-      const tagsEl = document.createElement('span');
-      tagsEl.classList.add('tag');
-      tagsEl.innerText= tag;
-      tagsEl.appendChild(tagEL);
+      const tagEl = document.createElement('span')
+      tagEl.classList.add('tag')
+      tagEl.innerText= tag;
+      tagsEl.appendChild(tagEl)
      });
 }
 
@@ -51,11 +53,8 @@ function pickRandomTag()
 function highlightTag(tag) {
  tag.classList.add('highlight')
 
-
 }
 
 function unHighlightTag(tag) {
     tag.classList.remove('highlight')
-   
-   
-   }
+}
